77479aaeeb94ea08679cf5c3eac6f8fe66cae5ec
Moskovki_Mastiukova <ritacorotkova@yandex.ru>
2022-12-07 09:23:50 +0300

    Initial commit

--- app/src/main/java/com/example/myapplicationmsk/Cat.java
+++ app/src/main/java/com/example/myapplicationmsk/Cat.java
@@ -3,22 +3,30 @@ package com.example.myapplicationmsk;
 import android.util.Log;
 
 public class Cat extends Animal {
-    private int age;
-     private String name;
+    protected int age;
+    String name;
 
     public Cat() {
 
     }
-    public Cat(int age, String name){
+
+    public Cat(int age, String name) {
         this.age = age;
         this.name = name;
     }
 
     public void talk() {
-        Log.i(" talk()",  "Meow! My name is" + name +", and I'm " + age + "years old.");
+        Log.i(" talk()", "Meow! My name is" + name + ", and I'm " + age + "years old.");
     }
 
-
+    public void talk(int age) {
+        Log.i(" talk()", "Meow! I'm " + age + "years old.");
 
 
     }
+    public void talk(String hello) {
+        Log.i(" talk()", "Meow!" + hello);
+
+
+    }
+}
--- app/src/main/java/com/example/myapplicationmsk/MainActivity.java
+++ app/src/main/java/com/example/myapplicationmsk/MainActivity.java
@@ -6,58 +6,15 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private
-    //package acces
-    //protected
-    //public
-
-    Cat myCat;
-    int z;
-    Cat murzik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        z = z++;
+       Cat vaska = new Cat();
+       vaska.talk("Meow, meow");
 
-        myCat = new Cat(4, "Puma");
-        myCat.talk();
-        //murzik.breathe();
-
-
-        Cat murzik = new Cat(2, "Murza");
-        murzik.talk();
-
-
-        Cat glasha = new Cat();
-        glasha.age = 1;
-        glasha.name = "Masha";
-        glasha.number0fLegs = 4;
-        Log.i("number0fLegs", String.valueOf(glasha.number0fLegs));
-        glasha.talk();
-
-        Puma puma = new Puma();
-        puma.name = "Leo";
-        puma.breathe();
-        puma.talk();
-        puma.isAlive = true;
-        Log.i("isAlive", String.valueOf(puma.isAlive));
-        Log.i("breathe()", "My name is" + puma.name + " and " + puma.breathe());
-    }
-
-    void method(double x, int y) {
-        int a;
-        a = 1;
-        a = y + 1;
-        double d = x * 2;
-        z = a + y;
-    }
-
-    void method1() {
-        int a;
-        a = 1;
 
     }
 }
