6b3872e77cb749812705e88b0d97d4c8518b501a
Moskovki_Mastiukova <ritacorotkova@yandex.ru>
2022-12-16 13:19:23 +0300

    Анонимные классы

--- app/src/main/java/com/example/myapplicationmsk/MainActivity.java
+++ app/src/main/java/com/example/myapplicationmsk/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplicationmsk;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.*;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -24,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
         Cat cat1 = new Cat();
         cat1.catchMouse(1);
         cat1.catchMouse(3);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Cat singingCat = new Cat(){
+            @Override
+            public void talk(){
+                Log.i("talk()",  "I'm singing! La-la-la!");
+            }
+
+        };
+        singingCat.talk();
+        Cat cat = new Cat();
+        cat.talk();
     }
 
 }
